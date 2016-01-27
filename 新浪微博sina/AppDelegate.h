cacbfd9c8541d984b75d7d15bbd132d6e07e8285
zheng <zheng@zhengtekiMac.local>
2016-01-27 21:19:08 +0800

    Initial Commit

---
+++ 新浪微博sina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新浪微博sina
+//
+//  Created by zheng on 16/1/27.
+//  Copyright (c) 2016年 zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新浪微博sina/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新浪微博sina
+//
+//  Created by zheng on 16/1/27.
+//  Copyright (c) 2016年 zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
